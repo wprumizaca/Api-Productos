0aa359f483d4f2fdf6782f1315676028fa0dfe19
wprumizaca <wperezrumizaca@gmail.com>
2023-04-04 18:05:09 +0200

    Añadida libreria para poder usar getmapping, Web. Se crean métodos CRUD pero no hay control de respuesta todavía (200, 404, etc). Se modifica properties para poder usar script schema y data sql para no tener que meter los datos a mano

--- src/main/java/org/example/expose/ProductController.java
+++ src/main/java/org/example/expose/ProductController.java
@@ -3,13 +3,16 @@ package org.example.expose;
 import lombok.RequiredArgsConstructor;
 import org.example.persistence.entities.Product;
 import org.example.persistence.repositories.ProductRepository;
-//import org.springframework.web.bind.annotation.*;
+import org.hibernate.annotations.SQLInsert;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 // @RequestBody Permite inyectar el cuerpo de la petición en un objeto
 // @PathVariable Nos permite inyectar un fragmento de la URL en una variable
 
+
+@RestController //hace la conversion de lo que da el objeto a lo que verá el cliente (json, xml, etc)
 
 //@RequiredArgsConstructor
 public class ProductController {
@@ -21,16 +24,48 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
-    //Devolver todos los productos
 
-    //@GetMapping("/product")
+    //Devolver todos los productos
+    @GetMapping("/product")
     public List<Product> productList(){
-        return null;
+
+        return productRepository.findAll();
     }
 
+
     //Devolver uno específico
-    //@GetMapping("/product/{id}")
-//    public Product product(@PathVariable Long id){
-//        return null;
-//    }
+    @GetMapping("/product/{id}")
+    public Product product(@PathVariable Long id){
+        return productRepository.findById(id).orElse(null); //esto puede ser opcional. Entra en juego manejo de errores
+    }
+
+    //Crear producto
+    @PostMapping("/product")
+    public Product createproduct(@RequestBody Product nuevo){
+        return productRepository.save(nuevo);
+    }
+
+    //Modificar producto
+    @PutMapping("/product/{id}")
+    public Product modifiedproduct(@RequestBody Product modificar,@PathVariable Long id){
+        if(productRepository.existsById(id)){
+            modificar.setId(id);
+            return productRepository.save(modificar);
+        }else {
+            return null;
+        }
+    }
+
+    //Borrar un producto
+    @DeleteMapping("/product/{id}")
+    public Product deleteproduct(@PathVariable Long id){
+        if(productRepository.existsById(id)){
+            Product product = productRepository.findById(id).get();
+            productRepository.deleteById(id); //Para borrar el producro
+            return product; //para que nos devuelva el producto borrado
+        }else {
+            return null;
+        }
+
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into product (id, nombre, precio) values (default, 'Juice - Orange, Concentrate', 91);
+insert into product (id, nombre, precio) values (default, 'Beef - Ground, Extra Lean, Fresh', 87);
+insert into product (id, nombre, precio) values (default, 'Cheese - Parmesan Grated', 39);
+insert into product (id, nombre, precio) values (default, 'Cups 10oz Trans', 67);
+insert into product (id, nombre, precio) values (default, 'Wine - Beringer Founders Estate', 27);
+insert into product (id, nombre, precio) values (default, 'Bread - Wheat Baguette', 82);
+insert into product (id, nombre, precio) values (default, 'Quail - Eggs, Fresh', 3);
+insert into product (id, nombre, precio) values (default, 'Cheese - Mascarpone', 97);
+insert into product (id, nombre, precio) values (default, 'Mace', 25);
